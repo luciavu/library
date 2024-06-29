0d47dee918b4c1c32c7cfc8791791d7a49bcf349
Lucia Vu <luciavu6@outlook.com>
2024-06-30 03:48:47 +1000

    Update add book feature

--- script.js
+++ script.js
@@ -28,6 +28,16 @@ function removeBookFromLibrary(bookId) {
     console.log('remove book');
 }
 
+function toggleModal() {
+    let modal = document.querySelector('.modal');
+    if (modal.style.display == "" || modal.style.display == "none") {
+        resetImagePreview();
+        modal.style.display = 'flex';
+    } else {
+        modal.style.display = 'none';
+    } 
+}
+
 function updateLibrary() {
     for (let book in myLibrary) {
         console.log(book);
@@ -44,3 +54,36 @@ function toggleActive(button) {
         target.classList.add('active');
     }
 }
+function resetImagePreview() {
+    const previewImage = document.getElementById('previewImage');
+    const preview = document.getElementById('imagePreview');
+    preview.style.display = 'none';
+    previewImage.src = '#';
+}
+
+function addEventListeners() {
+    // Image selection + preview
+    const input = document.getElementById('imageUpload');
+    const preview = document.getElementById('imagePreview');
+    const previewImage = document.getElementById('previewImage');
+
+    input.addEventListener('change', function() {
+        const file = this.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function() {
+                preview.style.display = 'block';
+                previewImage.src = reader.result;
+            };
+
+            reader.readAsDataURL(file);
+        } else {
+            preview.style.display = 'none';
+            previewImage.src = '#';
+        }
+    });
+}
+
+addEventListeners();
