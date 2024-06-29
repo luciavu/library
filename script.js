d3d50a2130a000bc5a1d528cbbb472218e699a76
Lucia Vu <luciavu6@outlook.com>
2024-06-30 00:40:50 +1000

    Add modal display

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// Temporary library storage method
+const myLibrary = [];
+let idCounter = 0;
+
+class Book {
+    constructor(id, title, bookCover, author, status, pages, liked) {
+        this.id = id;
+        this.title = title;
+        this.bookCover = bookCover;
+        this.author = author;
+        this.status = status;
+        this.pages = pages;
+        this.liked = liked;
+    }
+}
+
+function addBookToLibrary(title, bookCover, author, status, pages, liked) {
+    console.log('add book');
+    // Assign unique ID
+    let id = idCounter++;
+
+    // Check title not already exists
+    myLibrary.push(new Book(id, title, bookCover, author, status, pages, liked));
+}
+
+function removeBookFromLibrary(bookId) {
+    // Find book by ID
+    console.log('remove book');
+}
+
+function updateLibrary() {
+    for (let book in myLibrary) {
+        console.log(book);
+    }
+}
+
 function toggleActive(button) {
     let target = document.getElementById(button);
     if (target.classList.contains("active")) {
